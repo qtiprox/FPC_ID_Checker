1c0186ba35d48c292eb6a6c1c069276b03f6f63a
qtiprox <qtiprox@gmail.com>
2018-02-28 22:35:50 -0600

    Code Start

---
+++ FPC_ID_Check/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FPC_ID_Check.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
